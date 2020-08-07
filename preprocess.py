b2fa03f107eebdbee20da12fb5e0dabd22c2149e
crane719 <tmp.utils@gmail.com>
2020-08-08 02:29:50 +0900

    preprocessでresize, trim, dump

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,36 @@
 import hoge
 import cv2
 import random
+import joblib
 
 class Preprocess():
-    def __init__(self, content_dir, style_dir):
+    def __init__(self, content_dir, style_dir, save_dir):
         content_dirs = hoge.get_filedirs(content_dir+"/*")
-        style_dirs = hoge.get_filedirs(style_dir+"/*")
-
         print("content images: %d"%(len(content_dirs)))
-        print("style images: %d"%(len(style_dirs)))
-
-        for content_dir in content_dirs:
+        for i, content_dir in enumerate(content_dirs):
+            if i%1000==0:
+                print(" [%d/%d]"%(i, len(content_dirs)))
             pic = cv2.imread(content_dir)
             pic = cv2.resize(pic, (512,512))
             pic = self.random_trim(pic, 256)
+            pic = pic/256 # normalization
+            joblib.dump(pic, save_dir+"/content/"+"%d"%(i))
+            cv2.imwrite(save_dir+"/content/"+"%d.png"%(i), pic)
+
+        style_dirs = hoge.get_filedirs(style_dir+"/*")
+        print("style images: %d"%(len(style_dirs)))
+        for i, style_dir in enumerate(style_dirs):
+            if i%1000==0:
+                print(" [%d/%d]"%(i, len(style_dirs)))
+            pic = cv2.imread(style_dir)
+            pic = cv2.resize(pic, (512,512))
+            pic = self.random_trim(pic, 256)
+            pic = pic/256 # normalization
+            joblib.dump(pic, save_dir+"/style/"+"%d"%(i))
 
     def random_trim(self, pic, size):
         w = random.choice(range(0, pic.shape[1]))
         h = random.choice(range(0, pic.shape[0]))
-
         if w>=size:
             w_range = [w-size, w]
         else:
@@ -27,7 +39,6 @@ class Preprocess():
             h_range = [h-size, h]
         else:
             h_range = [h, h+size]
-
         pic = pic[:, w_range[0]:w_range[1], :]
         pic = pic[h_range[0]:h_range[1], :, :]
         return pic
@@ -35,5 +46,5 @@ class Preprocess():
 
 if __name__ == '__main__':
     from config import *
-    Preprocess(train_c_dir, train_s_dir)
+    Preprocess(train_c_dir, train_s_dir, train_dataset)
 
